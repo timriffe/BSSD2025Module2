0abc77d0e1057e801a7b46b409bfd7e255ce58a2
timriffe <tim.riffe@gmail.com>
2025-07-11 08:32:54 +0200

    .

--- 02_lifetables.R
+++ 02_lifetables.R
@@ -53,7 +53,7 @@ lt_full <- function(data, radix = 1, groups){
       # new use nx
       nx = c(diff(age),1),
       ax = mx_to_ax(mx = mx, 
-                    sex = groups$sex[1], 
+                    sex = rep(groups$sex,n())), 
                     age = age,
                     nx = nx),
       qx = calc_qx(mx = mx, 
